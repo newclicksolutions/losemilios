0450590cb6aedbff7e8234f497da1d39d614d8d2
luisk <lcaicedo@newclicksoluciones.com>
2023-12-14 15:51:29 -0500

    change port api

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   //enable CORS:
   app.enableCors();
   //set to use a dynamic port:
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
