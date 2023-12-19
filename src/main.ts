01143c86cc4e084f9e487997f19e7684d55951df
luisk <lcaicedo@newclicksoluciones.com>
2023-12-18 19:46:01 -0500

    muchos cambios

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   //enable CORS:
   app.enableCors();
   //set to use a dynamic port:
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
